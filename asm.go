5c4bb81091f0b06772b09337f972e3a902524d0f
kayos@tcp.direct <kayos@tcp.direct>
2024-06-04 23:35:22 -0700

    SIMD: attempt 2 (builds at least)

--- asm.go
+++ asm.go
@@ -1,4 +1,4 @@
-// //go:build ignore
+//go:build ignore
 
 package main
 
@@ -49,19 +49,12 @@ func gp64() reg.Register {
 	return build.GP64()
 }
 
+func gp32() reg.Register {
+	return build.GP32()
+}
+
 func gp8() reg.Register {
 	return build.GP8()
-}
-
-func arg(name string) gotypes.Component {
-	if name == "" {
-		panic("r u ok")
-	}
-	return build.Param(name)
-}
-
-func load(src gotypes.Component, dst reg.Register) reg.Register {
-	return build.Load(src, dst)
 }
 
 func store(src reg.Register, dst gotypes.Component) {
@@ -70,10 +63,6 @@ func store(src reg.Register, dst gotypes.Component) {
 
 func define(funcName string, argName string, argType string, ret string) {
 	build.TEXT(funcName, attr.NOSPLIT, fmt.Sprintf("func(%s %s) %s", argName, argType, ret))
-}
-
-func ptr(base reg.Register) operand.Mem {
-	return operand.Mem{Base: base}
 }
 
 func zero(reg reg.Register) {
@@ -86,7 +75,7 @@ func zero(reg reg.Register) {
 		build.XORL(reg, reg)
 	case 64:
 		build.XORQ(reg, reg)
-	case 256:
+	case 128, 256:
 		// AVX2: vector register zeroing
 		build.VXORPS(reg, reg, reg)
 	}
@@ -127,11 +116,12 @@ func constant(value uint64) operand.Constant {
 
 func main() {
 	define("ChecksumAVX2", "data", "[]byte", "uint16")
-	data := ptr(load(arg("data"), gp64()))
-	length := load(arg("data").Len(), gp64())
+	input := build.Param("data")
+	data := operand.Mem{Base: build.Load(input.Base(), gp64())}
+	length := build.Load(input.Len(), gp64())
 
 	// ===================================================
-	/*			   REGISTER INITIALIZATION:			    */
+	/*              REGISTER INITIALIZATION:            */
 	// ===================================================
 
 	// 64-bit register for sum
@@ -152,7 +142,7 @@ func main() {
 	// ---------------------------------------------------
 
 	// ===================================================
-	/*  	      		   MAIN LOOP:	  			    */
+	/*                      MAIN LOOP:                  */
 	lbl("loop") // =======================================
 
 	// if index >= length: goto remainder
@@ -176,13 +166,13 @@ func main() {
 	build.VPADDW(vectorSum, vectorSum, vectorData)
 
 	// index += 32 bytes
-	build.ADDQ(index, operand.U64(32))
+	build.ADDQ(operand.U32(32), index)
 
 	// goto loop label to continue accumulating data
 	jump(to(loop))
 
 	// ===================================================
-	/* 			  REMAINDER && REMAINDER LOOP:          */
+	/*            REMAINDER && REMAINDER LOOP:          */
 	lbl(remainder) // ====================================
 
 	remaining := gp64()
@@ -198,10 +188,10 @@ func main() {
 	testqjz(remaining, to(fin))
 
 	// load odd byte into 8-bit register
-	byteData := gp8()
-	build.MOVB(byteData, data.Idx(index, 1))
+	byteData := gp64()
+	build.MOVB(byteData.(reg.GPVirtual).As8(), data.Idx(index, 1))
 	// add byte to sum
-	build.ADDQ(sum, byteData)
+	build.ADDQ(byteData, sum)
 	// index++
 	build.INCQ(index)
 	// remaining--
@@ -210,7 +200,7 @@ func main() {
 	jump(to(rLoop))
 
 	// ===================================================
-	/*	 			  FINAL WRAP-AROUND:	            */
+	/*                 FINAL WRAP-AROUND:               */
 	lbl(fin) // ==========================================
 
 	// -----
@@ -218,36 +208,44 @@ func main() {
 	// -----
 
 	// 256 --> 128
-	// 128 bit vector register
 	unpacked128 := build.XMM()
-	// extract high 128-bits of vectorSum into unpacked
+	// extract high 128-bits of vectorSum into unpacked128
 	build.VEXTRACTI128(constant(1), vectorSum, unpacked128)
 	// add unpacked to vectorSum, storing result in vectorSum
-	build.VPADDW(vectorSum, vectorSum, unpacked128)
+	build.VPADDW(vectorSum.AsY(), vectorSum.AsY(), unpacked128.AsY())
+
+	build.VEXTRACTI128(constant(0), vectorSum, unpacked128)
+	build.VPADDW(vectorSum.AsY(), vectorSum.AsY(), unpacked128.AsY())
 
 	// 128 --> 64
-	// 64 bit register
-	unpacked64 := build.GP64()
-	// mov packed doubleword (vectorSum) integers to quadword (unpacked64)
-	build.VPMOVZXDQ(unpacked64, vectorSum)
-	// add unpacked64 to sum (both 64-bit registers)
-	build.ADDQ(sum, unpacked64)
+	tmp128 := build.XMM()
+	build.VMOVDQU(tmp128, vectorSum.AsX())
+
+	tmp64 := gp64()
+	// mov lower 32 bits of tmp128 to tmp64
+	build.MOVD(tmp64, tmp128)
+	build.ADDQ(sum, tmp64)
+	// (P)acked (S)hift (R)ight (L)ogical (D)ouble (Q)uadword
+	build.PSRLDQ(constant(8), tmp128.AsX())
+	// mov lower 32 bits of tmp128 to tmp64
+	build.MOVD(tmp128, tmp64)
+	// add tmp64 to sum
+	build.ADDQ(sum, tmp64)
 
 	// 64 --> 32
-	// (P)acked (S)hift (Right) (L)ogical (D)ouble (Q)uadword
-	// shift vectorSum (256 bits) right by 8 bytes (64 bits)
-	build.PSRLDQ(constant(8), vectorSum)
-	build.VPMOVZXDQ(unpacked64, vectorSum)
-	build.ADDQ(sum, unpacked64)
+	tmp32 := gp32()
+	build.MOVL(tmp32, sum.As32())
+	build.SHRL(constant(16), tmp32)
+	build.ADDL(sum.As32(), tmp32)
 
 	// 32 --> 16
-	shifted := gp64()
-	build.MOVQ(shifted, sum)
-	build.SHRQ(shifted, constant(16))
-	build.ADDQ(sum, shifted)
-	build.ANDQ(sum, constant(0xFFFF))
+	tmp16 := gp32()
+	build.MOVL(tmp16, tmp32)
+	build.SHRL(constant(16), tmp16)
+	build.ADDL(sum.As32(), tmp16)
+	build.ANDQ(operand.U32(0xFFFF), sum)
 
-	store(sum, build.ReturnIndex(0))
+	store(sum.As16(), build.ReturnIndex(0))
 
 	build.Generate()
 }
